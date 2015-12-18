8eb3e42146e6300200c52f80758e8f35d65b53bd
Fernando <correo@fernando.com.ar>
2015-12-18 13:29:10 -0300

    0.8.7, WIP, do not use.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Make signin and signout their own pages with routes (in javascript) with materialize CSS classes",
-  version: '0.8.6',
+  version: '0.8.7',
   name: "cypher:accounts-entry",
   git: 'https://github.com/f3rnando/meteor-accounts-entry.git',
 });
